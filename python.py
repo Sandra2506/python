551e93924279d3f4df898ddea05eefbb7f015da7
Sandra2506 <110335985+Sandra2506@users.noreply.github.com>
2022-08-04 20:03:42 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-print('Hi world')
-print('Hi world')
+def dank():
+    a = int(input('Ведите кол-во денег:'))
+    years = int(input("Введите кол-во лет:"))
+    r = 10
+
+    cnt = 0
+
+    if a > 0 :
+        for i in range(years):
+            a = int(a+r*a/100)
+            cnt += a
+        print('Сумма денег:',cnt)
+    else:
+        print('Введите положительное число!')
+
+dank()
